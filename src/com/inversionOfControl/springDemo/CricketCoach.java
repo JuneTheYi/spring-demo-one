02c0add43b7c7d93a0f37db2e39cedf702470191
June <junetheyi@gmail.com>
2022-06-29 18:19:54 -0400

    added properties file into applicationContext.xml to replace literal values injected into CricketCoach.java

--- src/com/inversionOfControl/springDemo/CricketCoach.java
+++ src/com/inversionOfControl/springDemo/CricketCoach.java
@@ -4,6 +4,29 @@ public class CricketCoach implements Coach {
 
 	private FortuneService fortuneService;
 	
+	// add new fields for emailaddress and team
+	private String emailAddress;
+	private String team;
+	
+	
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("diagnostic message to show that we're inside our setEmailAddress setter method");
+		this.emailAddress = emailAddress;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		System.out.println("diagnostic message to show that we're inside our setTeam setter method");
+		this.team = team;
+	}
+
 	// create a no-arg constructor
 	public CricketCoach() {
 		System.out.println("diagnostic message to show that we're inside our constructor when spring calls this no-arg constructor");
@@ -11,7 +34,7 @@ public class CricketCoach implements Coach {
 	
 	// setter method
 	public void setFortuneService(FortuneService fortuneService) {
-		System.out.println("diagnostic message to show that we're inside our setter method");
+		System.out.println("diagnostic message to show that we're inside our setFortuneService setter method");
 		this.fortuneService = fortuneService;
 	}
 
