c05dab6441ce57735845ec2ad2485ac75184668d
June <junetheyi@gmail.com>
2022-06-29 14:25:44 -0400

    spring bean container usage practice

--- src/com/inversionOfControl/springDemo/HelloSpringApp.java
+++ src/com/inversionOfControl/springDemo/HelloSpringApp.java
@@ -12,9 +12,11 @@ public class HelloSpringApp {
 		// retrieve bean from spring container
 		Coach theCoach = context.getBean("myCoach", Coach.class); // coach.class is an interface. TrackCoach implements the coach.class interface
 		// "myCoach" is the id given in applicationContext.xml for the bean
+		Coach newCoach = context.getBean("secondCoach", Coach.class);
 		
 		// call methods on the bean
 		System.out.println(theCoach.getDailyWorkout());
+		System.out.println(newCoach.getDailyWorkout());
 		
 		// close the context
 		context.close();
---
+++ src/com/inversionOfControl/springDemo/SwimmingCoach.java
@@ -0,0 +1,11 @@
+package com.inversionOfControl.springDemo;
+
+public class SwimmingCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return "swim 20 laps";
+	}
+
+}
